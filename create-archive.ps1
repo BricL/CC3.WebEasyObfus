afb81ffd4b13542fd59ca3fe70856150ca5615fd
ericlin09 <ericlin09@gmail.com>
2025-01-05 11:25:11 +0800

    Update create-archive.ps1 to include logo.jpg and switch to 7-Zip for compression; bump version to 1.0.1 in package.json

--- create-archive.ps1
+++ create-archive.ps1
@@ -8,7 +8,8 @@ $filesToZip = @(
     "README.md",
     "README-CN.md",
     "README.zh.md",
-    "README.en.md"
+    "README.en.md",
+    "logo.jpg"
 )
 
 # 定义压缩文件的名称
@@ -32,14 +33,25 @@ foreach ($item in $filesToZip) {
             Copy-Item -Path $item -Destination $destination
         }
     } else {
-        Write-Host "未找到：$item"
+        Write-Host "Can't find the: $item"
     }
 }
 
-# 压缩临时文件夹中的内容
-Compress-Archive -Path (Join-Path $tempFolder '*') -DestinationPath $zipFileName -Force
+# # 压缩临时文件夹中的内容
+# Compress-Archive -Path (Join-Path $tempFolder '*') -DestinationPath $zipFileName -Force
+
+# 使用 7-Zip 壓縮臨時文件夾中的內容
+$7zipPath = "C:\Program Files\7-Zip\7z.exe" # 7-Zip 的安裝路徑
+if (Test-Path $7zipPath) {
+    & $7zipPath a -tzip $zipFileName "$tempFolder\*" -mx=9 -mmt -r | Out-Null
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "Compressed Complete!: $zipFileName"
+    } else {
+        Write-Host "Compressed Failed, Error Code: $LASTEXITCODE"
+    }
+}
 
 # 删除临时文件夹
 Remove-Item -Path $tempFolder -Recurse -Force
 
-Write-Host "压缩完成！压缩文件为：$zipFileName"
+Write-Host "Mission Complete: $zipFileName"
